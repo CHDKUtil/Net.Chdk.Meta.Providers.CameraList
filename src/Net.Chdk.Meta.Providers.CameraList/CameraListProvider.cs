5e27bac2a2aa8b5cfc68fd0b60170b2ee4ce0094
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 19:51:16 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraList/CameraListProvider.cs
@@ -0,0 +1,27 @@
+﻿using Net.Chdk.Meta.Model.CameraList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Net.Chdk.Meta.Providers.CameraList
+{
+    sealed class CameraListProvider : ICameraListProvider
+    {
+        private IEnumerable<IInnerCameraListProvider> InnerProviders;
+
+        public CameraListProvider(IEnumerable<IInnerCameraListProvider> innerProviders)
+        {
+            InnerProviders = innerProviders;
+        }
+
+        public IDictionary<string, ListPlatformData> GetCameraList(string path, string categoryName)
+        {
+            var ext = Path.GetExtension(path);
+            var writer = InnerProviders.SingleOrDefault(w => w.Extension.Equals(ext, StringComparison.OrdinalIgnoreCase));
+            if (writer == null)
+                throw new InvalidOperationException($"Unknown camera writer extension: {ext}");
+            return writer.GetCameraList(path, categoryName);
+        }
+    }
+}
